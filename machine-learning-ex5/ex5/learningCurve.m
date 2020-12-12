8743eea6560a29edcaf79bfdede4b7157e343201
stathis94 <zephyroswest@gmail.com>
2020-12-13 00:25:04 +0200

    first commit

--- machine-learning-ex5/ex5/learningCurve.m
+++ machine-learning-ex5/ex5/learningCurve.m
@@ -53,8 +53,8 @@ error_val   = zeros(m, 1);
 
 % ---------------------- Sample Solution ----------------------
 
-for i=1:m
-    theta = trainLinearReg(X(1:i, :), y(1:i), lambda);
+for i=1:m % for each training set (1...i)
+    theta = trainLinearReg(X(1:i, :), y(1:i), lambda); %find theta that minimizes regularized linear J
     [error_train(i,:), grad] = linearRegCostFunction(X(1:i, :), y(1:i), theta, 0);
     [error_val(i,:), grad] = linearRegCostFunction(Xval, yval, theta, 0);
 end
---
+++ machine-learning-ex7/ex7/computeCentroids.m
@@ -0,0 +1,46 @@
+function centroids = computeCentroids(X, idx, K)
+%COMPUTECENTROIDS returns the new centroids by computing the means of the 
+%data points assigned to each centroid.
+%   centroids = COMPUTECENTROIDS(X, idx, K) returns the new centroids by 
+%   computing the means of the data points assigned to each centroid. It is
+%   given a dataset X where each row is a single data point, a vector
+%   idx of centroid assignments (i.e. each entry in range [1..K]) for each
+%   example, and K, the number of centroids. You should return a matrix
+%   centroids, where each row of centroids is the mean of the data points
+%   assigned to it.
+%
+
+% Useful variables
+[m n] = size(X);
+
+% You need to return the following variables correctly.
+centroids = zeros(K, n);
+
+
+% ====================== YOUR CODE HERE ======================
+% Instructions: Go over every centroid and compute mean of all points that
+%               belong to it. Concretely, the row vector centroids(i, :)
+%               should contain the mean of the data points assigned to
+%               centroid i.
+%
+% Note: You can use a for-loop over the centroids to compute this.
+%
+
+% idx is nx1 with each element value 1...K
+
+for k=1:K
+    cluster_members = [find(idx == k)]; % get the indexes of idx with value k
+    cluster_size = length(cluster_members);
+    centroids(k,:) = (1/cluster_size)*sum(X(cluster_members,:)); %VECTORIZED!!!!
+end
+
+
+
+
+
+
+% =============================================================
+
+
+end
+
